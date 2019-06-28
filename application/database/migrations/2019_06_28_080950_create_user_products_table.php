37e069f83e1d929f7a66925697cc003a23a0b43b
Muhammad Raza <muhammad.raza@jumprampgames.com>
2019-06-28 14:13:07 +0500

    add migrations for products & user_products table

---
+++ application/database/migrations/2019_06_28_080950_create_user_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_products', function (Blueprint $table) {
+            $table->integer('user_id', false, true);
+            $table->integer('product_id', false, true);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_products');
+    }
+}
